3bbbf54a9b1bfbf0e3b9ad53b05fbe116d033ef3
Dmitry Santalov <dmitsantalov@yandex.ru>
2024-05-07 17:11:10 +0300

    ver 1.1
    An incremental condition has been added

--- etl_service/Procedure/set_executable_procedure.sql
+++ etl_service/Procedure/set_executable_procedure.sql
@@ -15,19 +15,30 @@ declare
 	
 	tbl_name text;
 	clmn_list text;
+	
+	column_name_for_increment varchar(20);
 
 begin
 
 
-	select source_table, list_column
-		into tbl_name, clmn_list
+	select source_table
+		into tbl_name
 		from etl_service.replicate_information 
 	where id = id_table;
-
-
-	set_script := 'insert into etl_service.' || tbl_name || ' (' || clmn_list || ') (select ' ||  clmn_list || ' from source_hive.'|| tbl_name || ')';
 	
 	
+	clmn_list := etl_service.set_column_list(tbl_name);
+	
+	column_name_for_increment := set_field_increment(id_table);
+	
+	if column_name_for_increment isnull 
+		then 
+			set_script := 'insert into etl_service.' || tbl_name || ' (' || clmn_list || ') (select ' ||  clmn_list || ' from source_hive.'|| tbl_name || ')';
+	else if column_name_for_increment isnotnull 
+		then 
+			set_script := 'insert into etl_service.' || tbl_name || ' (' || clmn_list || ') (select ' ||  clmn_list || ' from source_hive.'|| tbl_name || 
+						  ' where '|| column_name_for_increment || ' > (select '|| column_name_for_increment || ' from etl_service.' || tbl_name '))';
+	end if;
 	
 	
 	
