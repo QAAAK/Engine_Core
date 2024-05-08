50df0e0cd884f1057fc2c9f8f43f88f9ce3b5fa4
Dmitry Santalov <dmitsantalov@yandex.ru>
2024-05-08 11:54:36 +0300

    ver 1.0

---
+++ monitoring_service/Procedure/logger_load_target_table.sql
@@ -0,0 +1,38 @@
+CREATE OR REPLACE FUNCTION monitoring_service.logger_load_target_table(table_id bigint, error_text text, count_rows int8, status)
+	RETURNS void
+	LANGUAGE plpgsql
+	VOLATILE
+AS $$
+	
+
+			
+declare	
+
+
+	
+
+begin
+
+
+	
+	update monitoring_service.load_logger 
+	set  quantity_rows = count_rows, message_text = error_text, last_status = status
+	where id = table_id;
+
+	
+	
+	
+
+	
+end;
+
+
+
+
+
+
+
+
+
+$$
+EXECUTE ON ANY;
