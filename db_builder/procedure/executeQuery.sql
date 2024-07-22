2d6709a873082db069a869f4e1aafc69a8971767
Dmitry Santalov <dmitsantalov@yandex.ru>
2024-07-22 16:34:12 +0300

    Add files via upload

---
+++ db_builder/procedure/executeQuery.sql
@@ -0,0 +1,69 @@
+CREATE OR REPLACE FUNCTION db_builder.executequery(query text)
+	RETURNS text
+	LANGUAGE plpgsql
+	VOLATILE
+AS $$
+	
+	
+	
+
+
+			
+declare	
+
+
+	quantityRows int8 := 0;
+
+	errorCode text;
+	errorText text;
+	messageErrorText text;
+
+	status text := 'PASSED';
+
+
+begin 
+	
+	begin
+	
+	
+		execute query;
+	
+	
+		GET  diagnostics
+		quantityRows = ROW_COUNT;
+		
+	
+		exception when others then 
+		
+			get STACKED diagnostics 
+		
+			errorCode = RETURNED_SQLSTATE,
+			errorText = MESSAGE_TEXT;
+			messageErrorText = errorCode || ' ' || errorText;
+		
+			status = 'FAILED';
+		
+			perform db_builder.writingLogTable(query, status, quantityRows, messageErrorText);
+			
+			return 'OK';
+
+	end;
+
+	perform db_builder.writingLogTable(query, status, quantityRows, messageErrorText);	
+
+return messageErrorText;
+
+end;
+
+
+
+
+
+$$
+EXECUTE ON ANY;
+
+-- Permissions
+
+ALTER FUNCTION db_builder.executequery(text) OWNER TO analyze_bi_owner;
+GRANT ALL ON FUNCTION db_builder.executequery(text) TO public;
+GRANT ALL ON FUNCTION db_builder.executequery(text) TO analyze_bi_owner;
---
+++ db_builder/procedure/writingLogTable.sql
@@ -0,0 +1,37 @@
+CREATE OR REPLACE FUNCTION db_builder.writinglogtable(query text, statusquery text, cntrows int8, messagetext text)
+	RETURNS void
+	LANGUAGE plpgsql
+	VOLATILE
+AS $$
+	
+	
+	
+
+
+			
+declare	
+
+operationQuery text;
+tableName text;
+
+
+begin 
+		
+		operationQuery := db_builder.typeOperations(query);
+		tableName := db_builder.getTableName(query);
+	
+	
+		INSERT INTO db_builder.log_dml_table (table_name, status, operation, quantity_rows, message_text, dttm)
+		VALUES(tableName, statusQuery , operationQuery , cntRows, messageText, clock_timestamp());
+
+end;
+
+
+
+$$
+EXECUTE ON ANY;
+
+-- Permissions
+
+ALTER FUNCTION db_builder.writinglogtable(text, text, int8, text) OWNER TO analyze_bi_owner;
+GRANT ALL ON FUNCTION db_builder.writinglogtable(text, text, int8, text) TO analyze_bi_owner;
