7bdeebc7bccb662afdd7c297f832e132b453641e
Dmitry Santalov <dmitsantalov@yandex.ru>
2024-07-22 13:45:42 +0300

    Add files via upload

---
+++ db_builder/procedure/getTableName.sql
@@ -0,0 +1,36 @@
+CREATE OR REPLACE FUNCTION db_builder.gettablename(query text)
+	RETURNS text
+	LANGUAGE plpgsql
+	VOLATILE
+AS $$
+	
+	
+	
+
+
+			
+declare	
+
+	tableName text;
+
+begin 
+	
+	tableName = SPLIT_PART(query, ' ', 3);
+	
+return tableName;
+
+end;
+
+
+
+
+
+
+
+$$
+EXECUTE ON ANY;
+
+-- Permissions
+
+ALTER FUNCTION db_builder.gettablename(text) OWNER TO analyze_bi_owner;
+GRANT ALL ON FUNCTION db_builder.gettablename(text) TO analyze_bi_owner;
---
+++ db_builder/procedure/typeOperations.sql
@@ -0,0 +1,34 @@
+CREATE OR REPLACE FUNCTION db_builder.typeoperations(query text)
+	RETURNS text
+	LANGUAGE plpgsql
+	VOLATILE
+AS $$
+	
+	
+	
+
+
+			
+declare	
+
+operation text;
+
+
+begin 
+		
+	operation = upper(substring(query, 1, 6));
+	
+return operation;
+
+end;
+
+
+
+$$
+EXECUTE ON ANY;
+
+-- Permissions
+
+ALTER FUNCTION db_builder.typeoperations(text) OWNER TO analyze_bi_owner;
+GRANT ALL ON FUNCTION db_builder.typeoperations(text) TO public;
+GRANT ALL ON FUNCTION db_builder.typeoperations(text) TO analyze_bi_owner;
