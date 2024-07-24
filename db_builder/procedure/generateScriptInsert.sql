2952f2d2571cbc4c917c360dcf89b64c9427fa9d
Dmitry Santalov <dmitsantalov@yandex.ru>
2024-07-24 17:18:19 +0300

    Add files via upload

---
+++ db_builder/procedure/generateScriptInsert.sql
@@ -0,0 +1,37 @@
+CREATE OR REPLACE FUNCTION db_builder.generatescriptinsert(schemaname text, tablename text)
+	RETURNS text
+	LANGUAGE plpgsql
+	VOLATILE
+AS $$
+	
+	
+	
+			
+declare	
+
+	cntRows bigint;
+	schemaNameAndTableName text;
+	insertQuery text;
+
+	
+begin
+	
+	schemaNameAndTableName = schemaName || '.' || tableName;
+	
+	insertQuery = 'insert into ' || schemaNameAndTableName || ' select * from ' || schemaName || '.<tableName>';
+	
+	return insertQuery;
+	
+
+end;
+
+
+
+
+$$
+EXECUTE ON ANY;
+
+-- Permissions
+
+ALTER FUNCTION db_builder.generatescriptinsert(text, text) OWNER TO analyze_bi_owner;
+GRANT ALL ON FUNCTION db_builder.generatescriptinsert(text, text) TO analyze_bi_owner;
