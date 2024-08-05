7be2aabe5cfa546f3d5892a3ab8986a4b77f801f
Dmitry Santalov <dmitsantalov@yandex.ru>
2024-08-05 12:00:46 +0300

    Add files via upload

---
+++ db_builder/procedure/updateTime.sql
@@ -0,0 +1,58 @@
+CREATE OR REPLACE FUNCTION db_builder.updatetime(tablename text)
+	RETURNS void
+	LANGUAGE plpgsql
+	VOLATILE
+AS $$
+	
+	
+	
+
+
+
+			
+declare	
+
+currentDTTM timestamp := clock_timestamp() ;
+nextDTTM timestamp;
+transferDTTM text;
+
+
+begin 
+	
+	
+	select when_update  
+	into transferDTTM
+	from db_builder.information_tables_dds a
+	where a.table_name = tablename;
+
+	if transferDTTM = 'Ежедневно' 
+		then 
+			nextDTTM = currentDTTM + '1 day';
+	elsif transferDTTM = 'Еженедельно' 
+		then 
+			nextDTTM = currentDTTM + '1 week';
+	elsif transferDTTM = 'Ежемесячно' 
+		then nextDTTM = currentDTTM + '1 month';	
+	end if;
+
+	update db_builder.information_tables_dds 
+    set last_dttm = currentDTTM, next_dttm = nextDTTM
+    where table_name = tablename;
+
+	
+	
+end;
+
+
+
+
+
+
+
+$$
+EXECUTE ON ANY;
+
+-- Permissions
+
+ALTER FUNCTION db_builder.updatetime(text) OWNER TO santalovdv;
+GRANT ALL ON FUNCTION db_builder.updatetime(text) TO santalovdv;
