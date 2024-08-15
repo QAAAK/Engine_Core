e527407a9e238fbe5decaae49c1d6daf060ea301
Dmitry Santalov <dmitsantalov@yandex.ru>
2024-08-15 09:57:39 +0300

    Add files via upload

--- db_builder/DDL/information_source_and_target_connection.sql
+++ db_builder/DDL/information_source_and_target_connection.sql
@@ -1,9 +1,23 @@
-create table db_builder.information_source_and_target_connection (
+-- db_builder.information_source_and_target_connection definition
 
-	id int8 NOT NULL DEFAULT nextval('db_builder.sequence_information_source_and_target_connection'::regclass),	
-	source_table text,
-	target_table text,
-	type_load varchar(50),
-	script text,
-	last_update text
-) distributed by (id)
+-- Drop table
+
+-- DROP TABLE db_builder.information_source_and_target_connection;
+
+CREATE TABLE db_builder.information_source_and_target_connection (
+	id int8 NOT NULL DEFAULT nextval('db_builder.sequence_information_source_and_target_connection'::regclass),
+	source_table text NULL,
+	target_table text NULL,
+	type_load varchar(50) NULL,
+	script text NULL,
+	last_update timestamp NULL,
+	create_column varchar(20) NULL,
+	increment_column varchar(20) NULL
+)
+DISTRIBUTED BY (id);
+
+-- Permissions
+
+ALTER TABLE db_builder.information_source_and_target_connection OWNER TO santalovdv;
+GRANT ALL ON TABLE db_builder.information_source_and_target_connection TO santalovdv;
+GRANT ALL ON TABLE db_builder.information_source_and_target_connection TO analyze_bi_owner;
